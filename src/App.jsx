29cea7c56ebe5bb52bf3fad9ba2afb24dc64b39d
angelostori <angelostori13@gmail.com>
2026-03-23 10:25:04 +0100

    test API index route for products in React frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/products")
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
   return (
     <>
+      <div>
+        <h1>Prodotti</h1>
 
+        {products.map(product => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.price} €</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
