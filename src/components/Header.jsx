459b5db88a19a43b4a505f80f762372fe37736fd
angelostori <angelostori13@gmail.com>
2026-03-23 15:17:04 +0100

    componentize the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+
+    return (
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <div className="container">
+                <div className="nav navbar-nav">
+                    <Link className="nav-item nav-link active" to="/">
+                        Home
+                    </Link>
+                    <Link className="nav-item nav-link" to="/products">Prodotti</Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function AppLayout() {
 
     return (
         <div className="d-flex flex-column vh-100">
-            <header>
 
-            </header>
+            <Header />
+
             <main>
                 <Outlet />
             </main>
+
             <Footer />
+
         </div>
     )
 } 
