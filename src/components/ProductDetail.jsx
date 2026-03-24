cac7208c05af114005a5cccadc688b5565ae4882
angelostori <angelostori13@gmail.com>
2026-03-24 11:32:43 +0100

    implement the product details page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -7,7 +7,28 @@ export default function ProductDetails({ product }) {
                 <div className="card-body">
                     <h5 className="card-title">{product.name}</h5>
                     <p className="card-text">{product.description}</p>
-                    <p className="card-text"><strong>Prezzo:</strong> €{product.price}</p>
+                    <div className="d-flex justify-content-between">
+                        <p><strong>Prezzo:</strong> €{product.price}</p>
+
+                        <div className="d-flex justify-content-between">
+                            <div class="input-group me-2">
+                                <button className="input-group-text bg-dark text-white">
+                                    <i className="bi bi-dash"></i>
+                                </button>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    min={1}
+                                    value={1}
+                                    style={{ maxWidth: "50px" }} />
+                                <button className="input-group-text bg-dark text-white">
+                                    <i className="bi bi-plus"></i>
+                                </button>
+                            </div>
+
+                            <button className="btn btn-primary me-2">Aggiungi</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
